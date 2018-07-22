c0cd1885cd08c232766437b09568a290495f7b22
Markus Barenhoff <mbarenh@alios.org>
2018-07-22 15:01:47 +0200

    added (failing) test for isometric file de-/encode.

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,9 +2,12 @@
 
 import           Control.Lens.Fold
 import           Control.Lens.Operators
+
 import           Data.Bencoding
 import           Data.Bencoding.Types
 import qualified Data.ByteString        as BS
+import qualified Data.ByteString.Lazy   as BL
+import           Data.Maybe
 import qualified Data.Text              as T
 import qualified Data.Vector            as V
 import           Test.Hspec
@@ -43,12 +46,24 @@ prop_bvalue_iso_bin a = pure a == preview _BValue (_BValue # a)
 prop_bvalue_iso_bin_strict :: BValue -> Bool
 prop_bvalue_iso_bin_strict a = pure a == preview _BValueStrict (_BValueStrict # a)
 
+iso_file :: String -> SpecWith ()
+iso_file fp = describe ("test file " ++ fp) $ do
+  bs <- runIO $ BL.readFile fp
+  it "should de-/encode isometric" $
+    let v :: BValue
+        v = fromMaybe (error "error") . preview _BValue $ bs
+        bs' = _BValue # v
+    in bs `shouldBe` bs'
+
+
 main :: IO ()
-main = hspec $
-  describe "BValue" $ do
+main = hspec $ parallel $ do
+  describe "BValue" $ parallel $ do
     it "bvalue Prism should be isometric on same value" $
       property prop_bvalue_iso
     it "_BValue Prism should be isometric on same value" $
       property prop_bvalue_iso_bin
     it "_BValueStrict Prism should be isometric on same value" $
       property prop_bvalue_iso_bin_strict
+  describe "testing files" $ parallel $ do
+      iso_file "test/test.dict"
